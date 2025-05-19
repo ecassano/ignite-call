5a69fa30e4ca822f6965bbe1ef4800ec82f3e673
eduardo.sa <eduardo.sa@rio.rj.gov.br>
2025-05-19 15:55:35 -0300

    Formulario de cadastro

---
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -0,0 +1,54 @@
+import { Form, FormAnnotation } from "./styles";
+import { TextInput, Button, Text } from "@ignite-ui/react";
+import { ArrowRight } from "phosphor-react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const claimUsernameFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: "Mínimo 3 caracteres" })
+    .regex(/^([a-z\\-]+)$/i, {
+      message: "O usuário pode conter apenas letras e hífens",
+    })
+    .transform((username) => username.toLowerCase()),
+});
+
+type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>;
+
+export function ClaimUsernameForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(claimUsernameFormSchema),
+  });
+
+  async function handleClaimUsername(data: ClaimUsernameFormData) {
+    console.log(data);
+  }
+
+  return (
+    <>
+      <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+        <TextInput
+          size="sm"
+          prefix="ignite.com/"
+          placeholder="seu-usuario"
+          {...register("username")}
+        />
+        <Button size="sm" type="submit">
+          Reservar usuário
+          <ArrowRight />
+        </Button>
+      </Form>
+      <FormAnnotation>
+        <Text size="sm">
+          {errors.username?.message ?? "Digite o nome do usuário desejado"}
+        </Text>
+      </FormAnnotation>
+    </>
+  );
+}
---
+++ src/pages/home/components/ClaimUsernameForm/styles.ts
@@ -0,0 +1,22 @@
+import { styled, Box } from "@ignite-ui/react";
+
+export const Form = styled(Box, {
+  display: 'grid',
+  gridTemplateColumns: '1fr auto',
+  gap: '$2',
+  marginTop: '$4',
+  padding: '$4',
+  borderRadius: 8,
+  boxSizing: 'border-box',
+  border: '4px solid $gray800',
+  backgroundColor: '$gray900',
+
+  '@media(max-width: 600px)': {
+    gridTemplateColumns: '1fr',
+  },
+})
+
+export const FormAnnotation = styled('div', {
+  color: '#f75a68',
+})
+
